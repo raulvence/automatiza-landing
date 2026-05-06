fef9562bf6f0c22112fe6a7b022202228d5c407b
raulvence <raulvence@gmail.com>
2026-05-06 18:00:11 -0500

    feat: sitemap, llms.txt para indexacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://automatiza.com.pa',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
